e00824358e6762f9daaccf9f60b72e1d9108dc91
ironwally <ironwally@ratfort.de>
2025-05-30 14:15:18 +0200

    lab1 half done. solution to fix lab1 directly in description of lab2

--- 0_TicTacToe.py
+++ 0_TicTacToe.py
@@ -1,5 +1,5 @@
 
-
+from random import randint
 game_state = ["_" for i in range(0, 9)]
 
 
@@ -11,14 +11,38 @@ def print_board():
 
 def update_board(position, symbol):
     # Task: Check if the position is valid (0-8)
+    if i not in range(0,9):
+        print("error not in range")
+        return 1
     # Task: Check if the position is empty
+    if game_state[position] != "_":
+        print("error position not empty")
+        return 1
     # otherwise return with a invalid message to the user
     game_state[position] = symbol
+    return 0
 
 
 def game_finished():
     # Write logic to decide if game is finished
     # and print the result ("win", "loose", "draw")
+    winning_positions = [
+        (0, 1, 2), (3, 4, 5), (6, 7, 8),  # horizontal
+        (0, 3, 6), (1, 4, 7), (2, 5, 8),  # vertical
+        (0, 4, 8), (2, 4, 6)              # diagonal
+    ]
+
+    for a, b, c in winning_positions:
+        if game_state[a] == game_state[b] == game_state[c] and game_state[a] != "_":
+            if game_state[a] == "x":
+                print("win")
+            else:
+                print("loose")
+            return True
+
+    if "_" not in game_state:
+        print("draw")
+        return True
     return False
 
 
@@ -31,6 +55,16 @@ if __name__ == "__main__":
     print_board()
     while not game_finished():
         i = int(input("Where do you want to put your 'x'? (0-8)"))
-        update_board(i, "x")
+        error = update_board(i, "x")
+        while (error):
+            print_board()
+            i = int(input("Where do you want to put your 'x'? (0-8)"))
+            error = update_board(i, "x")
         # Task: implement the opponents move
+        # opponent does random move
+        y = randint(0, 8)
+        error = update_board(y, "o")
+        while (error):
+            y = randint(0, 8)
+            error = update_board(y, "o")
         print_board()
--- 1_FrozenLake_Random.py
+++ 1_FrozenLake_Random.py
@@ -1,22 +1,68 @@
+# maximize return per episode
+# Run multiple exact same episodes
+# improve policy after each episode
+# policy = probability for each action saved per each state
+# actions = states that can be transitioned to from another state 
+# return = estimation of sum of rewards to be collected until end of episode
+# reward = reward for entering state
+# state = situation the agent can be in at time t, defining possible actions, possible reward
+# possible policy = change probability if one action gives higher result than other
+
 import gymnasium as gym
 import random
 
-env = gym.make("FrozenLake-v1", is_slippery=False, render_mode="ansi")
+
+class Episode:
+    def __init__(self):
+        self.actions = []
+        self.states = []
+    def __repr__(self): 
+        return "Episode actions:%s (%s) states:%s (%s)" % (self.actions, len(self.actions), self.states, len(self.states))
+
+
+env = gym.make("FrozenLake-v1", is_slippery=True, render_mode="ansi") # ansi or human
 
 random.seed(0)
+
+num_actions = 4 # in all states
+num_states = 16
+
+environment_states = [[0.25]*num_actions]*num_states
+
+def policy_improver(episode):
+    for i, state in enumerate(episode.states):
+        action = environment_states[state][episode.actions[i]]
+        #environment_states[state][episode.actions[i]] = environment_states[#state that action leads to#]
 
 print("## Frozen Lake ##")
 
 action2string = {0: "Left", 1: "Down", 2: "Right", 3: "Up"}
 
-episode_done = False
-state = env.reset(seed=0)
+for i in range(0,10):
+    episode_count = 0
+    reward = 0
+    while not reward>0:
+        episode_count+=1
+        episode_done = False
+        state = env.reset(seed=0)
+        state = 0
+        print("Start state:")
+        print(state)
+        print(env.render()) 
+        episode = Episode()
+        while not episode_done:
+            episode.states.append(state)
+            action = random.choices(range(num_actions), weights=environment_states[state])[0]
+            episode.actions.append(action)
+            state, reward, episode_done, _, _ = env.step(action)
+            print(f"\nAction:{action2string[action]}, new state:{state}, reward:{reward}")
+            print(env.render())
+        print("-----------------------------------")
+        policy_improver(episode)
 
-print("Start state:")
-print(env.render())
 
-while not episode_done:
-    action = random.randint(0, 3)  # choose a random action
-    state, reward, episode_done, _, _ = env.step(action)
-    print(f"\nAction:{action2string[action]}, new state:{state}, reward:{reward}")
-    print(env.render())
+    print(f"Episode count: {episode_count}")
+    print(episode)
+    #policy_improver(episode)
+
+    print("#################################")
